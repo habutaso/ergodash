e6e5044734e1ea9a822cf3587e5bd0e54f64a013
habutaso <habutaso@gmail.com>
2020-08-16 18:29:54 +0900

    change from eisu to enjp

--- keymap.c
+++ keymap.c
@@ -11,6 +11,7 @@ enum custom_keycodes {
   LOWER,
   RAISE,
   ADJUST,
+  M_ENJP,
 };
 
 #define EISU LALT(KC_GRV)
@@ -30,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |      | Raise|      | Adjst|      |   N  |   M  |   ,  |   .  |   /  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Ctrl |  GUI |  ALt | EISU |||||||| Space| Enter| Shift|||||||| Lower| Bksp | Space|||||||| Left | Down |  Up  | Right|
+   * | Ctrl |  GUI |  ALt | ENJP |||||||| Space| Enter| Shift|||||||| Lower| Bksp | Space|||||||| Left | Down |  Up  | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
@@ -38,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL ,                        KC_PSCR, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             RAISE,         ADJUST,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_LCTL, KC_LGUI, KC_LALT, EISU,             KC_SPC,  KC_ENT , KC_LSFT,       LOWER,  KC_BSPC, KC_SPC,           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+    KC_LCTL, KC_LGUI, KC_LALT, M_ENJP,           KC_SPC,  KC_ENT , KC_LSFT,       LOWER,  KC_BSPC, KC_SPC,           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
   ),
 
   /* Lower
@@ -51,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   6  |   7  |   8  |   9  |   0  |      | Raise|      | Adjst|      |   N  |   M  |   <  |   >  |   ?  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Ctrl |  GUI |  ALt | EISU |||||||| Space| Enter| Shift|||||||| Lower| Bksp | Space|||||||| Home |PageDn|PageUp|  End |
+   * | Ctrl |  GUI |  ALt | ENJP |||||||| Space| Enter| Shift|||||||| Lower| Bksp | Space|||||||| Home |PageDn|PageUp|  End |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_LOWER] = LAYOUT(
@@ -59,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_LCBR,                        KC_RCBR, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, \
     KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL ,                        KC_PSCR, KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_DQT , \
     KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,            RAISE,          ADJUST,          KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT, \
-    KC_LCTL, KC_LGUI, KC_LALT, EISU,             KC_SPC,  KC_ENT,KC_LSFT,            LOWER,KC_BSPC,  KC_SPC,         KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
+    KC_LCTL, KC_LGUI, KC_LALT, M_ENJP,           KC_SPC,  KC_ENT,KC_LSFT,            LOWER,KC_BSPC,  KC_SPC,         KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
   ),
 
   /* Raise
@@ -72,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   6  |   7  |   8  |   9  |   0  |      | Raise|      | Adjst|      |   N  |   M  |   <  |   >  |   ?  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Ctrl |  GUI |  ALt | EISU |||||||| Space| Enter| Shift|||||||| Lower| Bksp | Space|||||||| Home |PageDn|PageUp|  End |
+   * | Ctrl |  GUI |  ALt | ENJP |||||||| Space| Enter| Shift|||||||| Lower| Bksp | Space|||||||| Home |PageDn|PageUp|  End |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_RAISE] = LAYOUT(
@@ -80,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD, KC_EXLM, KC_MS_U, KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_WH_L, KC_WH_D, KC_WH_R, KC_WH_R, KC_RPRN, KC_PIPE, \
     KC_TAB,  KC_MS_L, KC_MS_D, KC_MS_R, KC_4,    KC_5,    KC_DEL ,                        KC_BSPC, KC_H,    KC_BTN1, KC_BTN3, KC_L,    KC_COLN, KC_DQT , \
     KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    RAISE  ,                        ADJUST , KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT, \
-    KC_LCTL, KC_LGUI, KC_LALT, EISU,             KC_SPC,  KC_ENT,KC_LSFT,          LOWER,KC_BSPC , KC_SPC,           KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
+    KC_LCTL, KC_LGUI, KC_LALT, ENJP,             KC_SPC,  KC_ENT,KC_LSFT,          LOWER,KC_BSPC , KC_SPC,           KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
   ),
 
   /* Adjust
@@ -151,6 +152,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+	case M_ENJP:
+	  if (record->event.pressed) {
+	  	SEND_STRING(SS_LGUI(" "));
+	  } else {
+	  	unregister_code(KC_LANG1);
+	  }
+	  return false;
+	  break;
   }
   return true;
 }
